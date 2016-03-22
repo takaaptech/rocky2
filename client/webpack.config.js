6de1bacafb1946be8a40069e1d33da8decbbd901
cswleo <cswleo@gmail.com>
2016-03-22 16:45:12 +0800

    framework

---
+++ client/webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  context: path.resolve(__dirname, 'src/js'),
+
+  cache: true,
+
+  entry: {
+    app: 'index.js',
+    vendor: ['lodash']
+  },
+
+  output: {
+    path: path.resolve('./build/js'),
+    publicPath: '/js/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ],
+    noParse: [/\.min\.js/]
+  },
+
+  resolve: {
+    root: [
+      path.join(__dirname, 'src/js')
+    ],
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js'],
+    alias: []
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
+  ]
+};
+
+module.exports = config;

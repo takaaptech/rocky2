84a5de103fb38b2a866f17789244854095a36cdd
cswleo <cswleo@gmail.com>
2016-03-24 09:11:18 +0800

    xxx

--- client/src/js/components/Poker.js
+++ client/src/js/components/Poker.js
@@ -7,14 +7,32 @@ export default class Lobby extends Component {
   constructor(props) {
     super(props);
     this.game = null;
-    pomelo.init({ host: 'pomelo', port: 3010, log: true });
+
+    pomelo.init({
+      host: 'pomelo',
+      port: 3010,
+      log: true
+    }, () => {
+      pomelo.request('connector.entryHandler.enter', {
+        username: 'Leo',
+        rid: '001'
+      }, (data) => {
+        if (data.error) {
+          console.log('error', data);
+          return;
+        }
+        console.log('success')
+      });
+    });
   }
 
-  //test() {
-  //  pomelo.request('connector.entryHandler.entry', (data) => {
-  //    console.log(data);
-  //  });
-  //}
+  test() {
+    pomelo.request('poker.pokerHandler.send', { username: 'Leo', rid: '000' }, (data) => {
+      console.log('success');
+    }, () => {
+      console.log('error')
+    });
+  }
 
   componentDidMount() {
     this.game = new Game(800, 600, Phaser.AUTO, this.refs.canvas, {
@@ -35,6 +53,7 @@ export default class Lobby extends Component {
     return (
       <div>
         <h1>Poker</h1>
+        <button onClick={ this.test.bind(this) }>TT</button>
         <div ref="canvas"></div>
       </div>
     )
